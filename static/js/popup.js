b00baeee225c834433b2fe665f1196762d9b4546
Ben <bencabe93@gmail.com>
2021-02-18 13:36:37 +0000

    started work on creating add meal modal

--- static/js/popup.js
+++ static/js/popup.js
@@ -1,24 +1,26 @@
-// Get the modal
-var modal = document.getElementById("myModal");
-
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+// Get the modals
+var ingredient_modal = document.getElementById("new-ingredient-modal");
+var meal_modal = document.getElementById("new-meal-modal");
 
 // When the user clicks on the button, open the modal
-function popUpToggle() {
-  modal.style.display = "block";
-//   form = document.getElementById('new-ingredient-form').innerHTML.value
-//   document.getElementById('new-ingredient-popup').innerHTML = form
+function popUpToggle(id) {
+    if (id == 'new-ingredient-button'){
+        ingredient_modal.style.display = "block";
+    }
+    else if (id == 'new-meal-button'){
+        meal_modal.style.display = "block";
+    }
 }
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
+function closeModal() {
+    ingredient_modal.style.display = "none";
+    meal_modal.style.display = "none";
 }
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
+  if (event.target == ingredient_modal || event.target == meal_modal) {
+    ingredient_modal.style.display = "none";
+    meal_modal.style.display = "none";
   }
 }

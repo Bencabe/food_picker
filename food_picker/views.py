f5803500fc48341a62800c81031b3e80681bee0f
Ben <bencabe93@gmail.com>
2021-03-26 10:13:28 +0000

    added some comments and small css changes

--- food_picker/views.py
+++ food_picker/views.py
@@ -7,11 +7,13 @@ from .models import Ingredient, Meal
 from django.db.models import Count
 
 def index(request):
+    # used to serve up index.html
     add_ingredient_form = AddIngredientForm()
     add_meal_form = AddMealForm()
     return render(request, "index.html",{'ingredient_form' : add_ingredient_form, 'meal_form': add_meal_form})
 
 def process_ingredient_form(request):
+    # used to process the form for adding new ingredients
     if request.method == 'POST':
         form = AddIngredientForm(request.POST)
         print(form)
@@ -21,6 +23,7 @@ def process_ingredient_form(request):
         return redirect('/')
 
 def process_meal_form(request):
+    # used to process the form for adding new meals
     if request.method == 'POST':
         form = AddMealForm(request.POST)
         print(form)
@@ -32,6 +35,7 @@ def process_meal_form(request):
     return HttpResponse("Didn't work")
 
 def autocomplete_ingredient(request):
+    # used to provide list of potential ingredients in ingredient picker based on first few letters 
     search_qs = Ingredient.objects.filter(name__startswith=request.GET['search'])
     results = []
     for r in search_qs:
@@ -41,6 +45,7 @@ def autocomplete_ingredient(request):
 
 
 def find_meals(request):
+    # used to find appropriate meals based on ingredients chosen by user 
     ingredients = request.GET['ingredients']
     # split ingredients into list and remove trailing white spaces
     ingredient_list = [x.strip() for x in ingredients.split(',')]
